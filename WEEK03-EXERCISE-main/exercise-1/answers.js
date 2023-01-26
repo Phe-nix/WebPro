0f5857f2376f5a8eaa13388609cab1af9436492d
Phe-nix <88503925+Phe-nix@users.noreply.github.com>
2023-01-26 21:53:16 +0700

    Add files via upload

---
+++ WEEK03-EXERCISE-main/exercise-1/answers.js
@@ -0,0 +1,39 @@
+function sayHello () {
+    return "Hello world!"
+    // TODO: return คำว่า "Hello world!"
+}
+
+function isString (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น String ให้ return true, ถ้าไม่ใช่ return false
+    return typeof input === "string"
+}
+
+function isNumber (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Number ให้ return true, ถ้าไม่ใช่ return false
+    return typeof input === "number"
+}
+
+function isArray (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Array ให้ return true, ถ้าไม่ใช่ return false
+    return Array.isArray(input)
+}
+
+function isObject (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Object ให้ return true, ถ้าไม่ใช่ return false
+    if(typeof input === "object" && !Array.isArray(input) && input !== null){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function isFunction (input) {
+    // input อาจจะเป็น String, Array, Number, Object หรือ Function ก็ได้
+    // TODO: ถ้า input เป็น Function ให้ return true, ถ้าไม่ใช่ return false
+    return typeof input === "function"
+}
---
+++ WEEK03-EXERCISE-main/exercise-2/answers.js
@@ -0,0 +1,45 @@
+function convertADtoBE(input) {
+    // AD คือคริสดศักราย
+    // BE คือพุทธศักราช
+    //
+    // TODO: แปลงปีใน คริสตศักราช เป็น พุทธศักราช เช่น 2000 เป็น "พ.ศ. 2543"
+    // โดยให้เติมตัวอักษร พ.ศ. เข้าไปด้านหน้าด้วย
+    if (input < 0 || typeof input == "string") {
+        return "ไม่ถูกต้อง"
+    }
+    else {
+        let num = input + 543
+        return "พ.ศ. " + num
+    }
+}
+
+function evenOrOdd(input) {
+    // TODO: ให้ตรวจสอบว่าตัวเลข input เป็นเลขคู่หรือเลขคี่
+    if (input % 2 == 0) {
+        return "even"
+    }
+    else {
+        return "odd"
+    }
+}
+
+function getFullName(input) {
+    // TODO: ให้นำคำนำหน้าชื่อ ชื่อต้น นามสกุล มาต่อกัน
+    if (input.sex == "male"){
+        return "Mr. "+ input.firstName + " " + input.lastName
+    }
+    else{
+        return "Ms. "+ input.firstName + " " + input.lastName
+    }
+}
+
+function getFirstName(input) {
+    // TODO: ให้ทำการตัดนามสกุลออกโดยใช้ indexOf() และ substring()
+    let sub = input.substring(0, 5)
+    if (input.indexOf(" ")){
+        return sub.substring(0, input.indexOf(" "))
+    }
+    else{
+        return input.substring(0, 5)
+    }
+}
