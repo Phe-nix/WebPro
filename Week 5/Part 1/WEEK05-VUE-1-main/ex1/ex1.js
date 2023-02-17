fb26d6159747f8f4a7c1e2aec714f5c207e8dddc
unknown <spicedexdee123@gmail.com>
2023-02-17 11:22:42 +0700

    Week 5 part 1

---
+++ Week 5/Part 1/WEEK05-VUE-1-main/ex1/ex1.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#ex1',
+    data() {
+        return {
+            student_id : 64070047,
+            student_fname : 'ธวิชญ์',
+            student_lname : 'จาวิสูตร'
+        }
+    },
+})
